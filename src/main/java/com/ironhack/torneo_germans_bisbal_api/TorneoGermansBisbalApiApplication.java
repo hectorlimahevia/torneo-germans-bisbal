1b2480f57d4057b7f521c10da01e19438a9ea0b0
hlimahevia <hlimahevia@gmail.com>
2026-05-27 17:57:29 +0200

    First commit of the project

---
+++ src/main/java/com/ironhack/torneo_germans_bisbal_api/TorneoGermansBisbalApiApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.torneo_germans_bisbal_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorneoGermansBisbalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TorneoGermansBisbalApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ironhack/torneo_germans_bisbal_api/TorneoGermansBisbalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.torneo_germans_bisbal_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorneoGermansBisbalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
